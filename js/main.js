5321866077e6bb464525090154b5afa291fb8e96
ecate <gucicatalina@gmail.com>
2023-07-31 17:54:32 +0300

    add link for shoes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Menu
 const nav = document.querySelector('#nav')
 const navBtn = document.querySelector('#nav-btn')
 const navBtnImg = document.querySelector('#nav-btn-img')
@@ -9,3 +10,18 @@ navBtn.onclick = () => {
         navBtnImg.src="./img/icons/nav_open.svg"
     }
 }
+//Scroll
+
+const scroll =document.getElementById('up')
+let isCh = false
+console.log(scroll)
+window.addEventListener("scroll", (event) => {
+    if(!isCh && this.scrollY > 1000){
+        isCh = true
+        scroll.style.display = 'block'
+    }else if(isCh && this.scrollY < 900){
+        isCh = false
+        scroll.style.display = 'none'
+    }
+
+});
